e43ed032b2d08191ed49429182000253be1d7fa9
joykepler <joyjoykepler@gmail.com>
2024-06-27 18:35:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input';
+import Button from './components/Button';
 
-function App() {
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  }
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  }
+
+  const handleSumNumbers = () => {
+
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation("+")
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber)  
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleMinusNumbers = () => {
+
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation("-")
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber)  
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleEquals = () => {
+
+    if(!firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+          case '-':
+            handleMinusNumbers();
+            break;
+          default:
+          break;
+      }
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="x"></Button>
+          <Button label="/"></Button>
+          <Button label="C" onClick={handleOnClear}></Button>
+          <Button label="X"></Button>
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}></Button>
+          <Button label="8" onClick={() => handleAddNumber('8')}></Button>
+          <Button label="9" onClick={() => handleAddNumber('9')}></Button>
+          <Button label="-" onClick={handleMinusNumbers}></Button>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}></Button>
+          <Button label="5" onClick={() => handleAddNumber('5')}></Button>
+          <Button label="6" onClick={() => handleAddNumber('6')}></Button>
+          <Button label="+" onClick={handleSumNumbers}></Button>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}></Button>
+          <Button label="2" onClick={() => handleAddNumber('2')}></Button>
+          <Button label="3" onClick={() => handleAddNumber('3')}></Button>
+          <Button label="=" onClick={handleEquals}></Button>
+        </Row>
+    
+      </Content>
+    </Container>
   );
 }
 
